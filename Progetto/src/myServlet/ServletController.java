5bc1e17b39880644d135510e9d0267318d17f965
Jose <jmsaquilue@gmail.com>
2019-11-26 18:57:25 +0100

    Lo hecho hoy,
    Mañana más

---
+++ Progetto/src/myServlet/ServletController.java
@@ -0,0 +1,35 @@
+package myServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ServletController", urlPatterns = {"/ServletController"})
+public class ServletController extends javax.servlet.http.HttpServlet {
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+
+            out.println(request.getParameter("utente"));
+
+            out.println("Risposta a invocazione senza parametri");
+
+        }
+    }
+
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+
+}
