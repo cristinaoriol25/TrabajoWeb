0a80aa1d2ca419af49d91408cb85f36b0254d8c2
Jose <jmsaquilue@gmail.com>
2020-01-04 22:57:43 +0100

    init

--- Progetto/src/myServlet/ServletController.java
+++ Progetto/src/myServlet/ServletController.java
@@ -132,15 +132,16 @@ public class ServletController extends javax.servlet.http.HttpServlet {
 
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        dao.DAO.registerDriver();
         processRequest(request, response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        dao.DAO.registerDriver();
         processRequest(request, response);
     }
 
-
-
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        dao.DAO.registerDriver();
+    }
 }
